b76683092605bdde3f00e412a640f2547af11dda
Rajesh <rajeshthavudaboina@gmail.com>
2025-12-20 12:43:19 +0530

    updated scripts

--- userdata.sh
+++ userdata.sh
@@ -1,37 +1,67 @@
 #!/bin/bash
+set -e
 
-sudo apt-get update
-sudo apt install docker.io -y
-sudo usermod -aG docker ubuntu
+# --------------------------------------
+# Log user-data execution
+# --------------------------------------
+exec > /var/log/user-data.log 2>&1
 
-[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.30.0/kind-linux-amd64
-chmod +x ./kind
-sudo mv ./kind /usr/local/bin/kind
+echo "Starting EC2 bootstrap..."
 
+# --------------------------------------
+# System update & Docker
+# --------------------------------------
+apt-get update -y
+apt-get install -y docker.io curl
+
+usermod -aG docker ubuntu
+systemctl enable docker
+systemctl start docker
+
+# --------------------------------------
+# Install kind
+# --------------------------------------
+curl -Lo /usr/local/bin/kind https://kind.sigs.k8s.io/dl/v0.30.0/kind-linux-amd64
+chmod +x /usr/local/bin/kind
+
+# --------------------------------------
+# Install kubectl
+# --------------------------------------
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-chmod +x kubectl
-mkdir -p ~/.local/bin
-mv ./kubectl ~/.local/bin/kubectl
+install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+rm kubectl
 
+# --------------------------------------
+# Install Helm
+# --------------------------------------
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-4
 chmod 700 get_helm.sh
 ./get_helm.sh
+rm get_helm.sh
 
-curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh" | bash
-sudo mv kustomize /usr/local/bin/
+# --------------------------------------
+# Install Kustomize
+# --------------------------------------
+curl -s https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh | bash
+mv kustomize /usr/local/bin/
 
+# --------------------------------------
+# Increase inotify limits (Kubeflow requirement)
+# --------------------------------------
+sysctl -w fs.inotify.max_user_instances=2280
+sysctl -w fs.inotify.max_user_watches=1255360
 
-# Increase inotify limits (critical for Kubeflow)
-sudo sysctl fs.inotify.max_user_instances=2280
-sudo sysctl fs.inotify.max_user_watches=1255360
+cat <<EOF >> /etc/sysctl.conf
+fs.inotify.max_user_instances=2280
+fs.inotify.max_user_watches=1255360
+EOF
 
-# Make persistent
-echo 'fs.inotify.max_user_instances=2280' | sudo tee -a /etc/sysctl.conf
-echo 'fs.inotify.max_user_watches=1255360' | sudo tee -a /etc/sysctl.conf
+# --------------------------------------
+# Run cluster creation as ubuntu user
+# --------------------------------------
+su - ubuntu <<'EOF'
 
-
-cat <<EOF > kind-kubeflow.yaml
+cat <<YAML > ~/kind-kubeflow.yaml
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
 name: kubeflow
@@ -55,6 +85,10 @@ nodes:
   image: kindest/node:v1.33.1
 - role: worker
   image: kindest/node:v1.33.1
+YAML
+
+kind create cluster --config ~/kind-kubeflow.yaml
+
 EOF
 
-kind create cluster --config kind-kubeflow.yaml
+echo "EC2 bootstrap completed successfully"
